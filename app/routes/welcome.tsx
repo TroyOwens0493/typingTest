e0101368093bd89cc456411c7365473c04cbfb0f
TroyOwens0493 <TroyOwens0493@users.noreply.github.com>
2026-02-23 16:13:19 -0700

    cleanup login/signup redir logic

--- app/routes/welcome.tsx
+++ app/routes/welcome.tsx
@@ -5,16 +5,8 @@ import { Welcome } from "../views/welcome";
 export default function Home() {
     const navigate = useNavigate();
 
-    function login() {
-        navigate("/login");
-    }
-
-    function signUp() {
-        navigate("/signup");
-    }
-
     return <Welcome
-        login={login}
-        signUp={signUp}
+        login={() => navigate("/login")}
+        signUp={() => navigate("/signup")}
     />;
 }
