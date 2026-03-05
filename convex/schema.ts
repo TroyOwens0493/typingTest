943ad4717a8a1cfd5a8400b40c59e62e730d9f19
TroyOwens0493 <TroyOwens0493@users.noreply.github.com>
2026-03-05 08:57:07 -0700

    add convex to project

---
+++ convex/schema.ts
@@ -0,0 +1,32 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+    user: defineTable({
+        username: v.string(),
+        password: v.string(),
+        email: v.string(),
+        rank: v.string(),
+        level: v.number(),
+        gamesPlayed: v.number(),
+        gamesWon: v.number(),
+        averageWPM: v.number(),
+        peakWPM: v.number(),
+        totalTime: v.number(),
+        averageAccuracy: v.number(),
+        recentSessions: v.object({
+            time: v.string(),
+            wpm: v.number(),
+            accuracy: v.number(),
+            result: v.string(),
+            place: v.number(),
+        }),
+    }),
+    match: defineTable({
+        code: v.string(),
+        gamemode: v.string(),
+        maxPlayers: v.number(),
+        difficulty: v.string(),
+        visibility: v.string(),
+    }),
+});
